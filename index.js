d444f485aeb65264a411a9c8f6bc37c50848e6d2
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-03 12:06:07 +0100

    Let the user decide the limit and the rules

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-function fizzFezzBuzzBangBongReverse(limit) {
+const readline = require('readline-sync');
+
+function promptForLimit() {
+    let response;
+    do {
+        console.log('Please enter the number you\'d like to FizzBuzz up to');
+        response = +readline.prompt();
+    } while (isNaN(response));
+    return response;
+}
+
+function getEnabledRules() {
+    const commandLineArgs = process.argv.slice(2);
+
+    // If no arguments were supplied, default to all of the rules.
+    if (commandLineArgs.length === 0) {
+        return [3, 5, 7, 11, 13, 17];
+    } else {
+        return commandLineArgs.map(arg => +arg);
+    }
+}
+
+function fizzFezzBuzzBangBongReverse(limit, enabledRules) {
     const rules = [[3, 'Fizz'], [13, 'Fezz'], [5, 'Buzz'], [7, 'Bang'], [11, 'Bong']];
 
     for (let i = 1; i <= limit; i++) {
         // Get the basic components of the output for i
-        let components = rules.filter(([key, _]) => i % key === 0).map(([_, value]) => value);
+        let components = rules
+            .filter(([key, _]) => i % key === 0 && enabledRules.includes(key))
+            .map(([_, value]) => value);
 
         // Apply the additional 'Bong' rule.
         if (components.includes('Bong')) {
@@ -11,7 +35,7 @@ function fizzFezzBuzzBangBongReverse(limit) {
         }
 
         // Apply the reversal rule
-        if (i % 17 === 0) {
+        if (i % 17 === 0 && enabledRules.includes(17)) {
             components.reverse(); 
         }
 
@@ -19,4 +43,6 @@ function fizzFezzBuzzBangBongReverse(limit) {
     }
 }
 
-fizzFezzBuzzBangBongReverse(300);
+const limit = promptForLimit();
+const enabledRules = getEnabledRules();
+fizzFezzBuzzBangBongReverse(limit, enabledRules);
